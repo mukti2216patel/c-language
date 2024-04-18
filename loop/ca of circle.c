c3b77e5fea7942ac5e54aa6b4b04083bc2a9ce4a
mukti2216patel <mukti2216patel@gmail.com>
2024-04-19 01:41:07 +0530

    123

--- loop/ca of circle.c
+++ loop/ca of circle.c
@@ -4,7 +4,7 @@ int main()
 {
     int a,b;float c,d;
     scanf("%d%d",&a,&b);
-    printf("Area1 2 of the circle is :%f\n Area of traingle : %f\n",c =(float) 3.14*a*b,d =(float)1/2*a*b);
+    printf("Area1 2 3 of the circle is :%f\n Area of traingle : %f\n",c =(float) 3.14*a*b,d =(float)1/2*a*b);
 
     return 0;
 }
